4700c71df056bf79ad717c81428f2322f1dfb267
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-14 10:10:20 +0530

    Add files via upload

---
+++ Java8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SMV
+ */
+public class Java8 {
+    public static void main(String[] args){
+        int x=7;
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x);
+        
+        int i=8;
+        System.out.println(i);
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(i++);
+        System.out.println(i);
+        
+        
+        
+    }
+    
+}
---
+++ Java9.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SMV
+ */
+public class Java9 {
+    public static void main(String[] args){
+        int age=12;
+        
+        if(age>=18){
+            System.out.println("Major:Eligible to vote");
+    }
+        else{
+            System.out.println("Minor:Not Eligible to vote");
+        }
+        }
+    
+}
