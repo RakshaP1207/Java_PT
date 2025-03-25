930c90398eaac92b4e4d76a591287d32d32de5b4
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-25 11:49:14 +0530

    Add files via upload

---
+++ Java17.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java17;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            if(i%2==0){
+                System.out.print("x"+" ");
+        }
+            else{
+                System.out.print("o"+" ");
+            }
+    }
+    
+
