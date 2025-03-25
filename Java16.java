4a323798cd1fbdd13e379be7d6e129a7fa79fecf
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-25 11:41:17 +0530

    Add files via upload

---
+++ Java16.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java16;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n=5;
+        char ch='a';
+        for(int i=0;i<n;i++){
+            System.out.print(ch+" ");
+            ch++;
+            
+        
+    }
+        // TODO code application logic here
+    }
+    
+}
