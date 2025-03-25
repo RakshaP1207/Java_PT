e9dad14ffda9a0e40056263fa494e8a536eb2bcf
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-25 11:30:41 +0530

    Add files via upload

---
+++ Java13.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java13;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println("Java Developer");
+        }
+        // TODO code application logic here
+    }
+    
+}
