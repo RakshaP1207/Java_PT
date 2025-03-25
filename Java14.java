c2b9d8e248abb1968c7153ce84731b167b44fb4a
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-25 11:30:08 +0530

    Add files via upload

---
+++ Java14.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java14;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int k=5;k>=0;k--){
+            System.out.println("K");
+        }
+        // TODO code application logic here
+    }
+    
+}
