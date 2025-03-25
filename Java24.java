36a7a932da5c709a04520a3926af9d354b4b178c
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-25 12:40:25 +0530

    Add files via upload

---
+++ Java24.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java24;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Program Starts");
+        int n=5;
+        while(n>0)
+        {
+            System.out.println("AI Developer");
+            n--;
+        }
+        System.out.println("Program Ends");
+        // TODO code application logic here
+    }
+    
+}
