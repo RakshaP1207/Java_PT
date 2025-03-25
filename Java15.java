d58b524eebfca6655ce7da2c9100b2c2efa40596
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-25 11:35:58 +0530

    Add files via upload

---
+++ Java15.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java15;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int x=0;x<10;x=x+2){
+            System.out.println("Senior test Engineer");
+        }
+        // TODO code application logic here
+    }
+    
+}
