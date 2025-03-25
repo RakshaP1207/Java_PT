4bad9bc935a33b7ad8e7423827b70d3c9f670a5c
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-25 12:08:00 +0530

    Add files via upload

---
+++ Java20.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java20;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         char ch='a';
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+            
+            System.out.print(ch+" ");
+            ch++; }
+            ch='a';
+            System.out.println();
+        // TODO code application logic here
+    }
+    
+}
+}
