22dfa67b7d11d12bc0a69ab3768789b9d8e38c58
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-28 10:41:03 +0530

    Add files via upload

---
+++ Java31.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java31;
+
+/**
+ *
+ * @author SMV
+ */
+import java.util.Scanner;
+public class Java31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int a =scn.nextInt();
+        int b =scn.nextInt();
+        double c =scn.nextDouble();
+        System.out.println(a+" "+b+" "+c);
+        
+        // TODO code application logic here
+    }
+    
+}
