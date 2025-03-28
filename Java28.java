9b8198b4bc362afc3b36a2e9f4558614dde0d986
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-28 09:54:14 +0530

    Add files via upload

---
+++ Java28.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java28;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java28 {
+    static int disp(){
+        return 45;}
+    static char print(){
+        return 'a';
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Program Starts");
+        int res=disp();
+        System.out.println(res);
+        System.out.println(print());
+        System.out.println("Program Ends");        // TODO code application logic here
+    }
+    
+}
