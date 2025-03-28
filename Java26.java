42661aa61859e1ce5f5de49582408542f07b790f
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-28 09:37:25 +0530

    Add files via upload

---
+++ Java26.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java26;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java26 {
+    static void greetings(){
+        System.out.println("Happy Morning");
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Program Starts");
+     greetings();
+System.out.println("Program ends");
+    
+        // TODO code application logic here
+    }
+    
+}
+
