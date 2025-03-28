e258a2acad333600ad9235b0a06d6fe269683088
Raksha P <raksha.23cs112@sode-edu.in>
2025-03-28 09:46:30 +0530

    Add files via upload

---
+++ Java27.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java27;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java27 {
+    static void add(int a,int b){
+        System.out.println("Sum of a+b = " + (a+b));
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Progrma Starts");
+        add(10,2);
+        add(4,3);
+        add(7,9);
+        System.out.println("Program Ends");
+        // TODO code application logic here
+    }
+    
+}
