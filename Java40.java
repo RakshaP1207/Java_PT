3cebd890277425086efdf972287c2bd71ab5695e
Raksha P <raksha.23cs112@sode-edu.in>
2025-04-08 12:01:06 +0530

    Add files via upload

---
+++ Java40.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java40;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java40 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] arr={11,12,13,14,15};
+        for(int i=arr.length-1;i>=0;i--){
+        System.out.println(arr[i]);
+        
+    }
+        // TODO code application logic here
+    }
+    
+}
