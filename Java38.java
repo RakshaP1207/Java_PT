3e59149a191d6e4e9435df89d5f30527ba1e3a80
Raksha P <raksha.23cs112@sode-edu.in>
2025-04-08 11:38:39 +0530

    Add files via upload

---
+++ Java38.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java38;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java38 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char[] vowels={'a','e','i','o','u'};
+        for(int i=0;i<vowels.length;i++){
+            System.out.println(vowels[i]);
+        }
+        // TODO code application logic here
+    }
+    
+}
