4574cec08da8ab6dbe28b59a36e5c7398e943343
Raksha P <raksha.23cs112@sode-edu.in>
2025-04-08 11:29:01 +0530

    Add files via upload

---
+++ Java37.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java37;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java37 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=11;
+        arr[1]=12;
+        arr[2]=13;
+        arr[3]=14;
+        arr[4]=15;
+        System.out.println(arr[0]);
+         System.out.println(arr[1]);
+          System.out.println(arr[2]);
+           System.out.println(arr[3]);
+            System.out.println(arr[4]);
+        // TODO code application logic here
+    }
+    
+}
