b3c4f93f60499a9ba80d306cdd926704c9c936ab
Raksha P <raksha.23cs112@sode-edu.in>
2025-04-11 09:58:37 +0530

    Add files via upload

---
+++ Java50.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java50;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java50 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=5;
+        int square=a*a;
+        if(square%10==a){
+            System.out.println("Automorphic Number");
+            
+        }
+        else{
+            System.out.println("Not An Automorphic Number");
+        }
+        // TODO code application logic here
+    }
+    
+}
