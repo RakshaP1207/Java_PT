e0b5c0a555189246b6ec5e89b5b6af5dd6999255
Raksha P <raksha.23cs112@sode-edu.in>
2025-04-11 10:48:47 +0530

    Add files via upload

---
+++ Java53.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java53;
+
+/**
+ *
+ * @author SMV
+ */
+import java.util.Scanner;
+public class Java53 {
+      public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner scn=new Scanner(System.in);
+    int a =scn.nextInt();
+    int b =scn.nextInt();
+    while(b>0)
+    {
+    a++;
+    b--;
+    
+}
+    System.out.println(a);
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        // TODO code application logic here
+    }
+    
+}
