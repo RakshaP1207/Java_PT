a258cdccf9897ccf3301e2130e5a355cf816c0c3
Raksha P <raksha.23cs112@sode-edu.in>
2025-05-02 09:16:05 +0530

    Add files via upload

---
+++ Java59.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java59;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java59 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num=5;
+        int fact=1;
+        for(int i=num;i>=1;i--){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+        // TODO code application logic here
+    }
+    
+}
